b1107732360b7cf58bcaecb5a335f3a539130cd0
faimin <fuxianchao@gmail.com>
2023-07-27 14:21:42 +0800

    add launchOptions

--- ZDRouter/Classes/Router/ZDRContext.h
+++ ZDRouter/Classes/Router/ZDRContext.h
@@ -11,7 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ZDRContext : NSObject
 
-@property (nonatomic, copy, nullable) NSString *biz;
+@property (nonatomic, strong) NSDictionary *launchOptions;
 @property (nonatomic, strong, nullable) id extraObj;
 
 @end
