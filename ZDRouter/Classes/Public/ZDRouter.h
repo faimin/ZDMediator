69297872de4cb80a888b2f2eeac32ed59122f4da
faimin <fuxianchao@gmail.com>
2023-07-31 23:45:06 +0800

    update ZDRouter

--- ZDRouter/Classes/Public/ZDRouter.h
+++ ZDRouter/Classes/Public/ZDRouter.h
@@ -8,10 +8,22 @@
 #ifndef ZDRouter_h
 #define ZDRouter_h
 
-#if __has_include(<ZDRouter/ZDRouter-umbrella.h>)
-#import <ZDRouter/ZDRouter-umbrella.h>
-#elif __has_include("ZDRouter-umbrella.h")
-#import "ZDRouter-umbrella.h"
+#if __has_include("ZD1V1Router.h")
+
+#import "ZD1V1Router.h"
+#import "ZD1VMRouter.h"
+#import "ZDRCommonProtocol.h"
+#import "ZDRContext.h"
+#import "ZDRouterDefine.h"
+
+#elif __has_include(<ZDRouter/ZD1V1Router.h>)
+
+#import <ZDRouter/ZD1V1Router.h>
+#import <ZDRouter/ZD1VMRouter.h>
+#import <ZDRouter/ZDRCommonProtocol.h>
+#import <ZDRouter/ZDRContext.h>
+#import <ZDRouter/ZDRouterDefine.h>
+
 #endif
 
 #endif /* ZDRouter_h */
