36b42d6841d8c3b9abf6556c04f662db22873e6f
faimin <fuxianchao@gmail.com>
2026-03-25 11:42:05 +0800

    feat: add ZDMLock Swift wrapper for NSRecursiveLock

---
+++ Sources/Classes/Swift/Private/ZDMLock.swift
@@ -0,0 +1,15 @@
+// Sources/Classes/Swift/Private/ZDMLock.swift
+
+final class ZDMLock: @unchecked Sendable {
+    private let _lock = NSRecursiveLock()
+
+    func lock()   { _lock.lock() }
+    func unlock() { _lock.unlock() }
+
+    @discardableResult
+    func withLock<T>(_ body: () throws -> T) rethrows -> T {
+        _lock.lock()
+        defer { _lock.unlock() }
+        return try body()
+    }
+}
