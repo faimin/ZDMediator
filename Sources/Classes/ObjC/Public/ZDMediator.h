b4400dbc7e7210015a369859f4bf7ddb976daf6a
faimin <fuxianchao@gmail.com>
2026-03-25 11:56:30 +0800

    feat: update ZDMediator.h umbrella - remove deleted ObjC imports, note Swift types auto-exposed

--- Sources/Classes/ObjC/Public/ZDMediator.h
+++ Sources/Classes/ObjC/Public/ZDMediator.h
@@ -1,29 +1,19 @@
-//
 //  ZDMediator.h
-//  ZDMediator
-//
-//  Created by Zero.D.Saber on 2023/7/31.
-//
+//  ZDMediator 0.5.0
+//  核心 ObjC 类型通过此头文件暴露；Swift 类型（Mediator、MediatorContext 等）
+//  通过编译器自动生成的模块接口暴露，无需手动 import。
 
 #ifndef ZDMediator_h
 #define ZDMediator_h
 
-#if __has_include("ZDMOneForAll.h")
-
-#import "ZDMOneForAll.h"
-#import "ZDMCommonProtocol.h"
-#import "ZDMContext.h"
 #import "ZDMediatorDefine.h"
+#import "ZDMProxy.h"
 #import "ZDMBroadcastProxy.h"
 
-#elif __has_include(<ZDMediator/ZDMOneForAll.h>)
+// dispatch + registerResponder 变参接口
+#import "Mediator+Dispatch.h"
 
-#import <ZDMediator/ZDMOneForAll.h>
-#import <ZDMediator/ZDMCommonProtocol.h>
-#import <ZDMediator/ZDMContext.h>
-#import <ZDMediator/ZDMediatorDefine.h>
-#import <ZDMediator/ZDMBroadcastProxy.h>
-
-#endif
+// ObjC 调用宏（ZDMGetService 系列）定义在 ZDMediatorDefine.h 中
+// Swift 类型（ZDMOneForAll / ZDMContext 等）由编译器通过 @objc(ZDMOneForAll) 暴露
 
 #endif /* ZDMediator_h */
